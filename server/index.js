19392d49698c40370da94190e100107d52e4aa87
KatLeo27 <kathyjane.leo@gmail.com>
2025-09-25 22:23:41 +0530

    backend update

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-// server placeholder
+// spotify.js
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Read your environment variables
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const redirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
+
+// Scopes define the permissions your app will request
+const scopes = [
+  "playlist-read-private",
+  "playlist-read-collaborative",
+];
+
+// This function generates the Spotify login URL
+export const getSpotifyAuthUrl = () => {
+  return `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=code&show_dialog=true`;
+};
